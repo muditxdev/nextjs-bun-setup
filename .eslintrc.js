e0adb7d2a820f3dcedcd63e066c00187c1e42637
mudit-gohash <mudit.jain@gohashinclude.com>
2025-02-25 10:57:15 +0530

    lint rules updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-    extends: ["next/core-web-vitals", "plugin:react/recommended", "plugin:jsx-a11y/recommended", "prettier"],
-    plugins: ["react", "jsx-a11y", "import", "prettier"],
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
-      "import/order": ["error", { "newlines-between": "always" }],
-      "jsx-a11y/alt-text": "warn",
-      "jsx-a11y/no-autofocus": "warn",
+  extends: [
+    "next/core-web-vitals",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  plugins: ["react", "jsx-a11y", "import", "prettier"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "import/order": ["error", { "newlines-between": "always" }],
+    "jsx-a11y/alt-text": "warn",
+    "jsx-a11y/no-autofocus": "warn",
+  },
+  settings: {
+    next: {
+      rootDir: ["src/"],
     },
-    settings: {
-      next: {
-        rootDir: ["src/"],
-      },
-    },
-  };
-  
+  },
+};
